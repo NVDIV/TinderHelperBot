06dda3dcb31992e3842f31c8ec97098bd631ba1a
Nadiia Rybak <nvdiv5@gmail.com>
2024-12-02 02:02:20 +0200

    Update TinderBoltApp.java

--- src/main/java/com/javarush/telegram/TinderBoltApp.java
+++ src/main/java/com/javarush/telegram/TinderBoltApp.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 
 public class TinderBoltApp extends MultiSessionTelegramBot {
     public static final String TELEGRAM_BOT_NAME = "JavaRushTinderHelperBot";
-    public static final String TELEGRAM_BOT_TOKEN = "8074931830:AAFHV-6ag43C8zf8O0oCBKyJ8hWThNbSAWw";
+    public static final String TELEGRAM_BOT_TOKEN = "";
     public static final String OPEN_AI_TOKEN = "chat-gpt-token"; //TODO: додай токен ChatGPT у лапках
 
     public TinderBoltApp() {
